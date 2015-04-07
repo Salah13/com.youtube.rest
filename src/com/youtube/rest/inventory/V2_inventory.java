348c25dcfb9d527a04b975d3527532ad2beb7ff6
Salah13 <salah.hassnaoui@yahoo.com>
2015-04-07 15:19:11 +0200

    implementer V2_inventory

--- src/com/youtube/rest/inventory/V2_inventory.java
+++ src/com/youtube/rest/inventory/V2_inventory.java
@@ -6,11 +6,12 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.*;
 
 import org.codehaus.jettison.json.JSONArray;
 
-@Path("/v2 /inventory")
+import com.youtube.dao.Schema308Tube;
+
+@Path("/v2/inventory")
 public class V2_inventory {
 
 	@GET
@@ -19,16 +20,24 @@ public class V2_inventory {
 			throws Exception {
 
 		String returnString = null;
-		JSONArray json = new JSONArray();
+		JSONArray json = new JSONArray(); 
 
 		try {
+			if(brand==null){
+				return Response.status(400).entity("Error : Please specify brand for this search").build();
+			}
+			Schema308Tube dao = new Schema308Tube();
+			json = dao.queryReturnBrandCompte(brand);
+			returnString=json.toString();
+			
 
 		} catch (Exception e) {
 			e.printStackTrace();
-			return  Response.status(500).entity("server was not able to process your request").build();
+			return Response.status(500)
+					.entity("server was not able to process your request")
+					.build();
 		}
 
 		return Response.ok(returnString).build();
 	}
-
 }
